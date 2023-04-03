5c3a55327a82a693c69031f4879ecbde440274c4
Carol Pinheiro <caroline.bpinheiro@outlook.com>
2023-04-03 16:48:34 -0300

    feat: added while loop and string interpolation

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,14 +3,30 @@ print("Bem vinda ao jogo de adivinhação!")
 print("***************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: " )
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: " )
 
-print("Você digitou: ", chute_str)
+    print("Você digitou: ", chute_str)
 
-chute = int(chute_str)
+    chute = int(chute_str)
 
-if (numero_secreto == chute):
-    print("Você acertou")
-else:
-    print("Você errou")
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+
+    if (numero_secreto == chute):
+        print("Você acertou")
+    else:
+        if (maior):
+            print("Você errou, seu chute foi maior que o número secreto")
+        elif(menor):
+            print("Você errou, seu chute foi menor que o número secreto")
+
+    rodada = rodada + 1
+        
+print('Fim do jogo')
