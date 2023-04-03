35d7ba3da82d2db8e34d4d4f4156a292ea1ae103
Carol Pinheiro <caroline.bpinheiro@outlook.com>
2023-04-03 17:01:48 -0300

    feat: added iterator breakers

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,15 +4,19 @@ print("***************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
+
+for rodada in range(1, total_de_tentativas+1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: " )
+    chute_str = input("Digite o seu número entre 1 e 100: " )
 
     print("Você digitou: ", chute_str)
 
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
@@ -21,12 +25,13 @@ while(rodada <= total_de_tentativas):
 
     if (numero_secreto == chute):
         print("Você acertou")
+        break
     else:
         if (maior):
             print("Você errou, seu chute foi maior que o número secreto")
         elif(menor):
             print("Você errou, seu chute foi menor que o número secreto")
 
-    rodada = rodada + 1
+
         
 print('Fim do jogo')
